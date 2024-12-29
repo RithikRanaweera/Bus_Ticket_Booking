b5cfd3da5ccd2b266363f85ae485e441ef91cbf0
rithikranaweera <rithikranaweera004@gmail.com>
2024-12-29 13:14:52 +0530

    Done Services Section

--- busticketbooking/src/pages/home/Home.jsx
+++ busticketbooking/src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Hero from './hero/Hero'
+import Services from './services/Services'
 
 const Home = () => {
     return (
         <div className="w-full min-h-screen space-y-16 pd-16">
             {/* Hero */}
             <Hero/> 
+            {/*Services*/}
+            <Services/>
         </div>
     )
 }
