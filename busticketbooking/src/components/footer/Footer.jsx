205b6e073314c1d8a5ea157e24ff3a2ece1e64b2
rithikranaweera <rithikranaweera004@gmail.com>
2024-12-31 11:42:00 +0530

    Edit the Footer bar section

--- busticketbooking/src/components/footer/Footer.jsx
+++ busticketbooking/src/components/footer/Footer.jsx
@@ -8,12 +8,12 @@ import PaypalImg from "../../assets/paypal.png"
 import CreditCardImg from "../../assets/creditcard.png"
 const Footer = () => {
   return (
-    <div className='w-full h-auto bg-neutral-950 py-12 mt-8'>   
+    <div className='w-full h-auto py-12 bg-neutral-950'>   
       <RootLayout className="space-y-10">
         
         {/* Footer other content */}
-        <div className="w-full grid grid-cols-5 gap-8">
-           <div className="col-span-2 space-y-8 md:pr-10 pr-0">
+        <div className="grid w-full grid-cols-5 gap-8">
+           <div className="col-span-2 pr-0 space-y-8 md:pr-10">
             <div className="space-y-3">
             {/* Logo */}
             <Link to="/" className='text-6xl font-bold text-primary'>
@@ -22,99 +22,99 @@ const Footer = () => {
            
 
            {/* some description */ }
-           <p className="text-sm text-neutral-500 font-normal">
+           <p className="text-sm font-normal text-neutral-500">
            BusFinder is your reliable companion for seamless bus travel across Sri Lanka. Our platform connects passengers and bus operators, enabling convenient seat reservations, route exploration, and secure online payments. Designed for efficiency and simplicity, BusFinder ensures a stress-free travel planning experience for everyone.
            </p>
            </div>
 
                 {/* social links */}
-                <div className="w-full flex items-center gap-x-5">
+                <div className="flex items-center w-full gap-x-5">
 
-                    <div className="w-11 h-11 rounded-xl bg-neutral-800/40 hover:bg-primary flex items-center justify-center cursor-pointer ease-in-out duration-500">
+                    <div className="flex items-center justify-center duration-500 ease-in-out cursor-pointer w-11 h-11 rounded-xl bg-neutral-800/40 hover:bg-primary">
                     <FaInstagram className='w-5 h-5 text-neutral-50'/>
                     </div>
 
-                    <div className="w-11 h-11 rounded-xl bg-neutral-800/40 hover:bg-primary flex items-center justify-center cursor-pointer ease-in-out duration-500">
+                    <div className="flex items-center justify-center duration-500 ease-in-out cursor-pointer w-11 h-11 rounded-xl bg-neutral-800/40 hover:bg-primary">
                     <FaFacebook className='w-5 h-5 text-neutral-50'/>
                     </div>
 
-                    <div className="w-11 h-11 rounded-xl bg-neutral-800/40 hover:bg-primary flex items-center justify-center cursor-pointer ease-in-out duration-500">
+                    <div className="flex items-center justify-center duration-500 ease-in-out cursor-pointer w-11 h-11 rounded-xl bg-neutral-800/40 hover:bg-primary">
                     <FaYoutube className='w-5 h-5 text-neutral-50'/>
                     </div>
 
-                    <div className="w-11 h-11 rounded-xl bg-neutral-800/40 hover:bg-primary flex items-center justify-center cursor-pointer ease-in-out duration-500">
+                    <div className="flex items-center justify-center duration-500 ease-in-out cursor-pointer w-11 h-11 rounded-xl bg-neutral-800/40 hover:bg-primary">
                     <FaXTwitter className='w-5 h-5 text-neutral-50'/>
                     </div>
 
                 </div>
            </div>
            <div className="col-span-1 space-y-5">
-            <h1 className="text-lg text-neutral-100 font-semibold">
+            <h1 className="text-lg font-semibold text-neutral-100">
                 Quick Links
             </h1>
 
             <div className="space-y-2">
-                <Link to="/" className='block text-base text-neutral-500 hover:text-neutral-300 font-normal ease-in-out duration-300'>
+                <Link to="/" className='block text-base font-normal duration-300 ease-in-out text-neutral-500 hover:text-neutral-300'>
                 About Us
                 </Link>
 
-                <Link to="/" className='block text-base text-neutral-500 hover:text-neutral-300 font-normal ease-in-out duration-300'>
+                <Link to="/" className='block text-base font-normal duration-300 ease-in-out text-neutral-500 hover:text-neutral-300'>
                 My Account
                 </Link>
 
-                <Link to="/" className='block text-base text-neutral-500 hover:text-neutral-300 font-normal ease-in-out duration-300'>
+                <Link to="/" className='block text-base font-normal duration-300 ease-in-out text-neutral-500 hover:text-neutral-300'>
                 Reserve your ticket
                 </Link>
 
-                <Link to="/" className='block text-base text-neutral-500 hover:text-neutral-300 font-normal ease-in-out duration-300'>
+                <Link to="/" className='block text-base font-normal duration-300 ease-in-out text-neutral-500 hover:text-neutral-300'>
                 Create your account
                 </Link>
             </div>
            </div>
 
            <div className="col-span-1 space-y-5">
-           <h1 className="text-lg text-neutral-100 font-semibold">
+           <h1 className="text-lg font-semibold text-neutral-100">
                 Top Reserved Routes
             </h1>
 
             <div className="space-y-2">
-                <Link to="/" className='block text-base text-neutral-500 hover:text-neutral-300 font-normal ease-in-out duration-300'>
+                <Link to="/" className='block text-base font-normal duration-300 ease-in-out text-neutral-500 hover:text-neutral-300'>
                 Matara - Galle
                 </Link>
 
-                <Link to="/" className='block text-base text-neutral-500 hover:text-neutral-300 font-normal ease-in-out duration-300'>
+                <Link to="/" className='block text-base font-normal duration-300 ease-in-out text-neutral-500 hover:text-neutral-300'>
                 Matara - Colombo
                 </Link>
 
-                <Link to="/" className='block text-base text-neutral-500 hover:text-neutral-300 font-normal ease-in-out duration-300'>
+                <Link to="/" className='block text-base font-normal duration-300 ease-in-out text-neutral-500 hover:text-neutral-300'>
                 Colombo - Kandy
                 </Link>
 
-                <Link to="/" className='block text-base text-neutral-500 hover:text-neutral-300 font-normal ease-in-out duration-300'>
+                <Link to="/" className='block text-base font-normal duration-300 ease-in-out text-neutral-500 hover:text-neutral-300'>
                 Colombo - Jaffna
                 </Link>
             </div>
            </div>
 
            <div className="col-span-1 space-y-5">
-            <h1 className="text-lg text-neutral-100 font-semibold">
+            <h1 className="text-lg font-semibold text-neutral-100">
                 Support Links
             </h1>
 
             <div className="space-y-2">
-                <Link to="/" className='block text-base text-neutral-500 hover:text-neutral-300 font-normal ease-in-out duration-300'>
+                <Link to="/" className='block text-base font-normal duration-300 ease-in-out text-neutral-500 hover:text-neutral-300'>
                 Privacy Policy
                 </Link>
 
-                <Link to="/" className='block text-base text-neutral-500 hover:text-neutral-300 font-normal ease-in-out duration-300'>
+                <Link to="/" className='block text-base font-normal duration-300 ease-in-out text-neutral-500 hover:text-neutral-300'>
                 Terms &Conditions
                 </Link>
 
-                <Link to="/" className='block text-base text-neutral-500 hover:text-neutral-300 font-normal ease-in-out duration-300'>
+                <Link to="/" className='block text-base font-normal duration-300 ease-in-out text-neutral-500 hover:text-neutral-300'>
                 Help & support Center
                 </Link>
 
-                <Link to="/" className='block text-base text-neutral-500 hover:text-neutral-300 font-normal ease-in-out duration-300'>
+                <Link to="/" className='block text-base font-normal duration-300 ease-in-out text-neutral-500 hover:text-neutral-300'>
                 FaQs
                 </Link>
             </div>
@@ -125,19 +125,19 @@ const Footer = () => {
         <div className="w-full h-px bg-neutral-800/50"  />
 
         {/* Copyright */}
-        <div className="w-full flex items-center justify-between">
-            <p className="text-sm text-neutral-600 font-normal">
+        <div className="flex items-center justify-between w-full">
+            <p className="text-sm font-normal text-neutral-600">
                 Coppyright & Coppy; 2025. All rights reserved.
             </p>
             <div className="flex item-center gap-x-2">
                 <div className="">
-                    <img src={MasterCardImg} alt="" className="w-fit h-9 object-contain object-center" />
+                    <img src={MasterCardImg} alt="" className="object-contain object-center w-fit h-9" />
                 </div>
                 <div className="">
-                    <img src={PaypalImg} alt="" className="w-fit h-9 object-contain object-center" />
+                    <img src={PaypalImg} alt="" className="object-contain object-center w-fit h-9" />
                 </div>
                 <div className="">
-                    <img src={CreditCardImg} alt="" className="w-fit h-9 object-contain object-center" />
+                    <img src={CreditCardImg} alt="" className="object-contain object-center w-fit h-9" />
                 </div>
             </div>
         </div>
